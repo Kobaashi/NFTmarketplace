98ce18e4a3ae7b4ed2b3bf149811118ffd884587
Українець Іван <127292648+UkrainetsIvan@users.noreply.github.com>
2024-10-29 14:41:39 +0300

    made collection section

--- nftMarket/src/app/pages/home/home.component.ts
+++ nftMarket/src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {NavMenuComponent} from '../../components/nav-menu/nav-menu.component';
 import {HeroComponent} from './hero/hero.component';
+import {TrendingCollectionComponent} from './trending-collection/trending-collection.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavMenuComponent,
-    HeroComponent
+    HeroComponent,
+    TrendingCollectionComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
--- nftMarket/src/app/pages/home/trending-collection/trending-collection.component.ts
+++ nftMarket/src/app/pages/home/trending-collection/trending-collection.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {FirstUppercasePipe} from '../../../shared/pipe/first-uppercase.pipe';
 
 @Component({
   selector: 'app-trending-collection',
   standalone: true,
-  imports: [],
+  imports: [
+    FirstUppercasePipe
+  ],
   templateUrl: './trending-collection.component.html',
   styleUrl: './trending-collection.component.scss'
 })
 export class TrendingCollectionComponent {
+    dog: string = "/img/Dog.png";
+    cat: string = "/img/Cat.png";
+    bear: string = "/img/Bear.png";
+    fox: string = "/img/fox.png";
 
+    mushrum: string = "/img/mushrum.png";
+    FirstMushrum: string = "/img/first-mushrum.png";
+    SecondMushrum: string = "/img/three-mushrum.png";
+    avatar: string = "/img/avatarka.png";
+
+    robot0312: string = "/img/Robot0312.png";
+    robot0512: string = "/img/Robot0512.png";
+    robot0987: string = "/img/Robot0987.png";
+    robot: string = "/icons/justRobot.svg";
 }
