f0ab17afe0204a92cc5b771ba4a2278a7e31c1c7
Українець Іван <127292648+UkrainetsIvan@users.noreply.github.com>
2024-10-29 15:32:53 +0300

    made array objects for section top creators

--- nftMarket/src/app/pages/home/topcreators/topcreators.component.ts
+++ nftMarket/src/app/pages/home/topcreators/topcreators.component.ts
@@ -8,5 +8,78 @@ import { Component } from '@angular/core';
   styleUrl: './topcreators.component.scss'
 })
 export class TopcreatorsComponent {
-
+  creators = [
+    {
+      id: 1,
+      ava: "/icons/Jaydon.svg",
+      name: "Keepitreal",
+      total: 34.53
+    },
+    {
+      id: 2,
+      ava: "/icons/Ruben.svg",
+      name: "DigiLab",
+      total: 34.53
+    },
+    {
+      id: 3,
+      ava: "/icons/Alfredo.svg",
+      name: "GravityOne",
+      total: 34.53
+    },
+    {
+      id: 4,
+      ava: "/icons/Davis.svg",
+      name: "Juanie",
+      total: 34.53
+    },
+    {
+      id: 5,
+      ava: "/icons/Livia.svg",
+      name: "BlueWhale",
+      total: 34.53
+    },
+    {
+      id: 6,
+      ava: "/img/fox.png",
+      name: "Mr Fox",
+      total: 34.53
+    },
+    {
+      id: 7,
+      ava: "/icons/Phillip.svg",
+      name: "Shroomie",
+      total: 34.53
+    },
+    {
+      id: 8,
+      ava: "/icons/Maria.svg",
+      name: "Robotica",
+      total: 34.53
+    },
+    {
+      id: 9,
+      ava: "/icons/Kianna.svg",
+      name: "RustyRobot",
+      total: 34.53
+    },
+    {
+      id: 10,
+      ava: "/icons/Avatar.svg",
+      name: "Animakid",
+      total: 34.53
+    },
+    {
+      id: 11,
+      ava: "/icons/Lydia.svg",
+      name: "Dotgu",
+      total: 34.53
+    },
+    {
+      id: 12,
+      ava: "/icons/Joselyn.svg",
+      name: "Ghiblier",
+      total: 34.53
+    },
+  ]
 }
