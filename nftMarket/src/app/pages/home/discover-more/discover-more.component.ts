1b9cd36fdb22ccae907754e69848663b9f66f780
Українець Іван <127292648+UkrainetsIvan@users.noreply.github.com>
2024-10-30 12:43:26 +0200

    add more nfts section

--- nftMarket/src/app/pages/home/discover-more/discover-more.component.ts
+++ nftMarket/src/app/pages/home/discover-more/discover-more.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import {FirstUppercasePipe} from '../../../shared/pipe/first-uppercase.pipe';
 
 @Component({
   selector: 'app-discover-more',
   standalone: true,
-  imports: [],
+  imports: [
+    FirstUppercasePipe
+  ],
   templateUrl: './discover-more.component.html',
   styleUrl: './discover-more.component.scss'
 })
 export class DiscoverMoreComponent {
 
+  eye: string = "/icons/Eye.svg";
+
+    nfts = [
+      {
+        image: "/img/DistantGalaxy.png",
+        name: "distant galaxy",
+        authorAva: "/icons/Rayna.svg",
+        authorName: "MoonDancer",
+        price: 1.63,
+        highestBid: 0.33,
+      },
+      {
+        image: "/img/LifeOnEdena.png",
+        name: "life on edena",
+        authorAva: "/icons/Angel.svg",
+        authorName: "NebulaKid",
+        price: 1.63,
+        highestBid: 0.33,
+      },
+      {
+        image: "/img/AstroFiction.png",
+        name: "astroFiction",
+        authorAva: "/icons/Lydia.svg",
+        authorName: "Spaceone",
+        price: 1.63,
+        highestBid: 0.33,
+      },
+    ]
 }
