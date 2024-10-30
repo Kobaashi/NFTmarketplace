82fe696a90a86d595f79c6fb4064fd85077a4f29
Українець Іван <127292648+UkrainetsIvan@users.noreply.github.com>
2024-10-30 12:33:48 +0300

    add section categories

--- nftMarket/src/app/pages/home/browsecategories/browsecategories.component.ts
+++ nftMarket/src/app/pages/home/browsecategories/browsecategories.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import {FirstUppercasePipe} from '../../../shared/pipe/first-uppercase.pipe';
 
 @Component({
   selector: 'app-browsecategories',
   standalone: true,
-  imports: [],
+  imports: [
+    FirstUppercasePipe
+  ],
   templateUrl: './browsecategories.component.html',
   styleUrl: './browsecategories.component.scss'
 })
 export class BrowsecategoriesComponent {
-
+      categories = [
+        {
+          icon: "/icons/PaintBrush.svg",
+          image: "/img/art.png",
+          name: "art"
+        },
+        {
+          icon: "/icons/Swatches.svg",
+          image: "/img/IceCreamApe.png",
+          name: "collectibles"
+        },
+        {
+          icon: "/icons/MusicNotes.svg",
+          image: "/img/music.png",
+          name: "music"
+        },
+        {
+          icon: "/icons/Camera.svg",
+          image: "/img/photography.png",
+          name: "photography"
+        },
+        {
+          icon: "/icons/VideoCamera.svg",
+          image: "/img/LifeOnEdena.png",
+          name: "video"
+        },
+        {
+          icon: "/icons/MagicWand.svg",
+          image: "/img/utility.png",
+          name: "utility"
+        },
+        {
+          icon: "/icons/Basketball.svg",
+          image: "/img/sport.png",
+          name: "sport"
+        },
+        {
+          icon: "/icons/Planet.svg",
+          image: "/img/virtualworld.png",
+          name: "virtual worlds"
+        },
+      ]
 }
--- nftMarket/src/app/pages/home/home.component.ts
+++ nftMarket/src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import {HeroComponent} from './hero/hero.component';
 import {TrendingCollectionComponent} from './trending-collection/trending-collection.component';
 import {TopCreatorComponent} from '../../components/top-creator/top-creator.component';
 import {TopcreatorsComponent} from './topcreators/topcreators.component';
+import {BrowsecategoriesComponent} from './browsecategories/browsecategories.component';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,8 @@ import {TopcreatorsComponent} from './topcreators/topcreators.component';
     HeroComponent,
     TrendingCollectionComponent,
     TopCreatorComponent,
-    TopcreatorsComponent
+    TopcreatorsComponent,
+    BrowsecategoriesComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
