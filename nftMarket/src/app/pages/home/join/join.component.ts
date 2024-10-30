9b47e60cddb4afb7ba87a6d5a24eeca6dc6c35e1
Українець Іван <127292648+UkrainetsIvan@users.noreply.github.com>
2024-10-30 15:10:26 +0200

    add validation on form subscribe

--- nftMarket/src/app/pages/home/join/join.component.ts
+++ nftMarket/src/app/pages/home/join/join.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {FirstUppercasePipe} from '../../../shared/pipe/first-uppercase.pipe';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-join',
   standalone: true,
   imports: [
-    FirstUppercasePipe
+    FirstUppercasePipe,
+    ReactiveFormsModule
   ],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss'
@@ -13,4 +15,44 @@ import {FirstUppercasePipe} from '../../../shared/pipe/first-uppercase.pipe';
 export class JoinComponent {
     actronavt: string = "/img/astronavt.png";
     message: string = "/icons/message.svg";
+    successMessage: string = '';
+
+  form:FormGroup =new FormGroup({
+    email:new FormControl("",[Validators.required,this.customeEmailValidator])
+  })
+
+  getError(control:any) : string {
+    if(control.errors?.required && control.touched)
+      return 'Ви нічого не ввели!';
+    else if(control.errors?.emailError && control.touched)
+      return 'Будь ласка введіть правильну пошту!';
+    else return '';
+  }
+
+  customeEmailValidator(control:AbstractControl) {
+    const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,20}$/;
+    const value = control.value;
+    if(!pattern.test(value) && control.touched)
+      return {
+        emailError:true
+      }
+    else return null;
+  }
+
+  submitForm() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+    } else {
+      this.successMessage = 'Ви підписалися, очікуйте повідомлень!';
+
+      setTimeout(() => {
+        this.form.reset();
+        this.successMessage = '';
+      }, 3000);
+    }
+  }
+
+  constructor() { }
+
+
 }
