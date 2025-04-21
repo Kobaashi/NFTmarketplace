9a3afe3c4dad31415340a1c23eeee5c5ddea7a77
Koba_Yashy <149396138+Kobaashi@users.noreply.github.com>
2025-04-21 18:49:34 +0300

    delete unused import

--- nftMarketBack/src/app.module.ts
+++ nftMarketBack/src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
-import { MController } from './s/m/m.controller';
-import { UsersModule } from './users/users.module';
-import { UsersModule } from './users/users.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController, MController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
