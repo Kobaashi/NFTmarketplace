f1d70be9b3d00c8f317096806d372cc8cff1c721
Koba_Yashy <149396138+Kobaashi@users.noreply.github.com>
2025-04-28 20:22:12 +0300

    add folder with auth for it logic

--- nftMarketBack/src/app.module.ts
+++ nftMarketBack/src/app.module.ts
@@ -4,14 +4,17 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { NftModule } from './nft/nft.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb+srv://IvanUkrainets:UdeChnu@cluster0.yzjnh.mongodb.net/NFTMarketplace?retryWrites=true&w=majority&appName=Cluster0'),
     UsersModule,
     NftModule,
+    AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ nftMarketBack/src/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  public async register() {}
+
+  public async login() {}
+
+  public async logout() {}
+}
